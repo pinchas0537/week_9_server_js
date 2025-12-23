458036b556dd9ba1b975cba595c19ad947691e7e
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-12-23 20:35:05 +0200

    Completion for teh share of users in the project

--- Day2/cntrl/userC.js
+++ Day2/cntrl/userC.js
@@ -9,18 +9,63 @@ export const getAllUsers = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
-}
+};
 
-export const addUser = async (req,res)=>{
+export const addUser = async (req, res) => {
     try {
         const user = await readfile(path)
-        const newUser = {name: req.body.name, password :req.body.password}
+        const newUser = { name: req.body.name, password: req.body.password }
         if (req.body.name && req.body.password) {
             user.push(newUser)
             await writefile(path, user)
             res.json(newUser)
-        }else{res.status(400).json({"message":"Missing name or password!"})}
+        } else { res.status(400).json({ "message": "Missing name or password!" }) }
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
-}
+};
+
+export const editUserById = async (req, res) => {
+    try {
+        const read = await readfile(path)
+        const username = req.params.username;
+        const name = req.body.name;
+        const password = req.body.password;
+
+        const update = read.find(user => user.name == username
+        )
+        if (update) {
+            if (name) update.name = name
+            if (password) update.nickname = password
+            await writefile(path, read);
+            res.json({ "messege": "You have successfully edited." });
+        }
+        else {
+            res.status(404);
+            res.send();
+        }
+    } catch (error) {
+        res.status(500);
+        res.json({ error: error.message });
+    }
+};
+
+export const deleteUser = async (req,res) => {
+    try {
+        const read = await readfile(path)
+        const username = req.params.username;
+        const del = read.findIndex(user => user.name == username);
+        if (del != -1) {
+            read.splice(del, 1);
+            await writefile(path, read)
+            res.json({ "deleted": true });
+        }
+        else {
+            res.status(404);
+            res.json({});
+        }
+    } catch (error) {
+        res.status(500);
+        res.json({ error :error.message});
+    }
+};
--- Day2/routers/userR.js
+++ Day2/routers/userR.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { addUser, getAllUsers } from "../cntrl/userC.js";
+import { addUser, deleteUser, editUserById, getAllUsers } from "../cntrl/userC.js";
 
 const router = express.Router()
 
 router.get("/",getAllUsers)
 
 router.post("/",addUser)
+
+router.put("/:username", editUserById)
+
+router.delete("/:username",deleteUser)
 
 export default router
